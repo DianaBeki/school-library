3df19afe089b174fd52b44a4e66a092618e2ac7a
Diana Beki <dianabeki54@gmail.com>
2023-09-04 20:31:03 +0300

    Update file

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
-require './person'
-# class student inherite person class
+require_relative 'person'
+
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    '¯(ツ)/¯'
   end
 end

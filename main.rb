21517157ae86e65f36b0e4b3e39c8c9381b58611
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-09-11 14:15:53 +0200

    Refactor the code

--- main.rb
+++ main.rb
@@ -1,24 +1,24 @@
 require_relative 'app'
 require_relative 'option'
 
-app = App.new
+App.new
 
 def main(app)
-puts "Welcome to School Library App! \n\n"
+  puts "Welcome to School Library App! \n\n"
 
-loop do
-  puts 'Choose an option:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
+  loop do
+    puts 'Choose an option:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
 
-  
+    number = gets.chomp
+    break if number == 7
 
-  option(app, option)
+    option(app, number)
   end
 end
-

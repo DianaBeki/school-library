c9bdba2881c630081ee2243c8a500a12900f363c
Diana Beki <dianabeki54@gmail.com>
2023-09-11 17:58:17 +0300

    Refactor code

--- main.rb
+++ main.rb
@@ -1,24 +1,30 @@
 require_relative 'app'
 require_relative 'option'
 
-App.new
+app = App.new
+option = List.new
 
-def main(app)
-  puts "Welcome to School Library App! \n\n"
+class Main
+  def main(app, option)
+    puts "Welcome to School Library App!\n\n"
 
-  loop do
-    puts 'Choose an option:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
+    loop do
+      puts 'Choose an option:'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
 
-    number = gets.chomp
-    break if number == 7
-
-    option(app, number)
+      user_input = gets.chomp.to_i
+      if user_input == 7
+        print 'Thank you for using this app!'
+        break
+      end
+    end
   end
 end
+
+Main.new.main(app, option)
--- option.rb
+++ option.rb
@@ -1,18 +1,18 @@
-def option(app, number)
-  case number
-  when '1'
-    app.list_books
-  when '2'
-    app.list_persons
-  when '3'
-    app.create_person
-  when '4'
-    app.create_book
-  when '5'
-    app.create_rental
-  when '6'
-    app.list_rentals_by_person
-  when '7'
-    print 'Thank you for using this app!'
+class List
+  def option_list(app, user_input)
+    case user_input
+    when 1
+      app.list_books
+    when 2
+      app.list_people
+    when 3
+      app.create_person
+    when 4
+      app.create_book
+    when 5
+      app.create_rental
+    when 6
+      app.list_rentals
+    end
   end
 end

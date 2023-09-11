55a7fdc9abdb5a1c2250d58db2c5ba25404cb664
Diana Beki <dianabeki54@gmail.com>
2023-09-11 14:49:09 +0300

    Refactor code

---
+++ option.rb
@@ -0,0 +1,18 @@
+def option(app, option)
+case option
+when '1'
+  app.list_books
+when '2'
+  app.list_persons
+when '3'
+  app.create_person
+when '4'
+  app.create_book
+when '5'
+  app.create_rental
+when '6'
+  app.list_rentals_by_person
+when '7'
+  print 'Thank you for using this app!'
+  break
+end
